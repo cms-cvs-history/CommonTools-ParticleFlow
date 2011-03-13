fe005207bff9bec6bb0faafedaf6b78f9e06b476
cmsbuild <>
2011-03-13 15:19:28 +0000

    This commit was manufactured by cvs2git to create tag 'CommonTools- ParticleFlow-V00-01-01-02'.
    Sprout from master 2011-03-01 21:08:42 UTC Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch> 'add photon isolation'
    Cherrypick from master 2011-03-13 15:19:27 UTC David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch> 'remove now unused variable':
        plugins/PFPileUp.cc

--- plugins/PFPileUp.cc
+++ plugins/PFPileUp.cc
@@ -107,14 +107,10 @@ PFPileUp::chargedHadronVertex( const Handle<VertexCollection>& vertices, const P
   
   reco::TrackBaseRef trackBaseRef( pfcand.trackRef() );
   
-  unsigned nFoundVertex = 0;
   size_t  iVertex = 0;
   unsigned index=0;
   typedef reco::VertexCollection::const_iterator IV;
   for(IV iv=vertices->begin(); iv!=vertices->end(); ++iv, ++index) {
-//     cout<<(*iv).x()<<" "
-// 	<<(*iv).y()<<" "
-// 	<<(*iv).z()<<endl;
 
     const reco::Vertex& vtx = *iv;
     
@@ -129,18 +125,11 @@ PFPileUp::chargedHadronVertex( const Handle<VertexCollection>& vertices, const P
       // one of the tracks in the vertex is the same as 
       // the track considered in the function
       if(baseRef == trackBaseRef ) {
-	iVertex = index;
-	nFoundVertex++;
+	return VertexRef( vertices, iVertex);
       }	 	
     }
   } 
 
-  if( nFoundVertex == 1) {
-    return VertexRef( vertices, iVertex);
-  }
-  else if(nFoundVertex>1) assert(false);
-
-  assert( !iVertex );
 
   // no vertex found with this track. 
   // as a secondary solution, associate the closest vertex in z
