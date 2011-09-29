e42dc9c55abf16761833391abd75f284a4540346
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2011-09-29 07:51:53 +0000

    backport of the mods in cbern_isolation_29Sept11_b

--- python/Isolation/pfMuonIsolation_cff.py
+++ python/Isolation/pfMuonIsolation_cff.py
@@ -2,7 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 # iso deposits and isolation values, defined by the Muon POG
 
-from  RecoMuon.MuonIsolation.muonPFIsolation_cff import *
+# this file is temporarily moved to this package.
+# in 4_4_X, we load this file from the RecoMuon/MuonIsolation package
+from  CommonTools.ParticleFlow.Isolation.muonPFIsolation_cff import *
 
 # computing the isolation for the muons produced by PF2PAT, and not for reco muons
 sourceMuons = 'pfSelectedMuons'
