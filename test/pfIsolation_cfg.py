27116bd6d92b3062277ca0ce51ce8b11f7e15265
Tae Jeong Kim <sha1-32b43f0d0700c58cd86e2e757255aa5f208dff55@cern.ch>
2012-03-09 17:33:35 +0000

    pfIsolation tools. renamed PF2PAT into PFBRECO (backward compatibility has been kept)

---
+++ test/pfIsolation_cfg.py
@@ -0,0 +1,52 @@
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("PFISO")
+
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1000)
+)
+ 
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+    '/store/relval/CMSSW_4_2_6/RelValTTbar/GEN-SIM-RECO/START42_V12-v1/0006/12A43774-08A9-E011-AFDC-00248C0BE01E.root'
+    
+    ))
+   
+
+# Tae Jeong, could you please remove these from the PF2PAT sequence?
+# they are not used, and are creating problems in such kinds of manipulations:
+# process.pfElectronIsolationSequence.remove( process.pfElectronIsoDepositsSequence )
+# process.pfElectronIsolationSequence.remove( process.pfElectronIsolationFromDepositsSequence )
+
+# process.load("CommonTools.ParticleFlow.PFBRECO_cff")
+
+from CommonTools.ParticleFlow.Tools.pfIsolation import setupPFElectronIso, setupPFMuonIso
+process.eleIsoSequence = setupPFElectronIso(process, 'gsfElectrons')
+process.muIsoSequence = setupPFMuonIso(process, 'muons')
+
+process.p = cms.Path(
+    # process.pfNoPileUpSequence +
+    process.pfParticleSelectionSequence + 
+    process.eleIsoSequence + 
+    process.muIsoSequence
+    )
+
+# output ------------------------------------------------------------
+
+process.out = cms.OutputModule("PoolOutputModule",
+                               outputCommands = cms.untracked.vstring('keep *'),
+                               fileName = cms.untracked.string('pfIsolation.root')
+)
+
+process.outpath = cms.EndPath(
+    process.out 
+    )
+
+
+# other stuff
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 10
+
